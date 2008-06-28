dddd885a12ce1c50793d6cec4603f85795a31e49
Mattia Barbon <mbarbon@cpan.org>
2008-06-28 14:57:02 +0000

    Very basic support for scalar assignment with morphing to proper type.

--- lib/Language/P/Value/Scalar.pm
+++ lib/Language/P/Value/Scalar.pm
@@ -8,6 +8,16 @@ sub type { 5 }
 
 sub as_scalar { return $_[0] }
 
+sub assign {
+    my( $self, $other ) = @_;
+
+    # FIXME proper morphing
+    %$self = ();
+    bless $self, ref( $other );
+
+    $self->assign( $other );
+}
+
 sub assign_iterator {
     my( $self, $iter ) = @_;
 
