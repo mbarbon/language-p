46f61a8a1c866635ca62dd5f7c35e49c14c2b558
Mattia Barbon <mbarbon@cpan.org>
2008-07-19 13:12:02 +0000

    Convert Language::P to be an object and add minimal command-line handling to it.   Delay code generation until it is required (will need to handle use/BEGIN blocks in a special way).   Add minimal support for debugging the parse phase.

--- lib/Language/P.pm
+++ lib/Language/P.pm
@@ -2,6 +2,10 @@ package Language::P;
 
 use strict;
 use warnings;
+use base qw(Class::Accessor::Fast);
+
+__PACKAGE__->mk_ro_accessors( qw(runtime parser generator) );
+__PACKAGE__->mk_accessors( qw(program program_arguments) );
 
 our $VERSION = '0.01';
 
@@ -9,8 +13,26 @@ use Language::P::Parser;
 use Language::P::Generator;
 use Language::P::Runtime;
 
-sub run {
-    my( $class, @args ) = @_;
+sub new_from_argv {
+    my( $class, $argv ) = @_;
+    my $self = $class->SUPER::new;
+
+    $self->initialize;
+
+    if( @$argv ) {
+        if( $argv->[0] =~ /^-/ ) {
+            $argv = $self->process_command_line( $argv );
+        }
+
+        $self->program( $argv->[0] );
+        $self->program_arguments( [ @$argv[1 .. $#$argv] ] );
+    }
+
+    return $self;
+}
+
+sub initialize {
+    my( $self ) = @_;
 
     my $runtime = Language::P::Runtime->new;
     my $generator = Language::P::Generator->new( { runtime => $runtime } );
@@ -18,8 +40,39 @@ sub run {
                                              runtime   => $runtime,
                                              } );
 
-    my $code = $parser->parse_file( $args[0] );
-    $runtime->run_last_file( $code );
+    $self->{runtime} = $runtime;
+    $self->{generator} = $generator;
+    $self->{parser} = $parser;
+}
+
+sub process_command_line {
+    my( $self, $argv ) = @_;
+
+    local @ARGV = @$argv;
+
+    require Getopt::Long;
+
+    Getopt::Long::GetOptions
+      ( \my %args,
+        'D=s' => \my $debugging,
+        );
+
+    if( $debugging ) {
+        foreach my $deb_opt ( split /,/, $debugging ) {
+            if( $deb_opt eq 'parse_tree' ) {
+                $self->generator->set_debug( $deb_opt );
+            }
+        }
+    }
+
+    return [ @ARGV ];
+}
+
+sub run {
+    my( $self ) = @_;
+
+    my $code = $self->parser->parse_file( $self->program );
+    $self->runtime->run_last_file( $code );
 }
 
 1;
