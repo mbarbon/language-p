44a887219bad4d8661f614a33511a168bdfdec65
Mattia Barbon <mbarbon@cpan.org>
2008-07-19 22:12:00 +0000

    Implement reference stringification, fix embarrassing syntax error in reference module.
    git-svn-id: file:///Users/mbarbon/devel/perl5/p/SVNREPOSITORY/language_p/trunk@63 200a23a6-3bc1-4d4e-9293-5d7e3368f848

--- lib/Language/P/Value/Reference.pm
+++ lib/Language/P/Value/Reference.pm
@@ -27,28 +27,52 @@ sub assign {
 }
 
 sub dereference_scalar {
+    my( $self ) = @_;
+
     die unless $self->{reference}->isa( 'Language::P::Value::Scalar' );
     return $self->{reference};
 }
 
 sub dereference_hash {
+    my( $self ) = @_;
+
     die unless $self->{reference}->isa( 'Language::P::Value::Hash' );
     return $self->{reference};
 }
 
 sub dereference_array {
+    my( $self ) = @_;
+
     die unless $self->{reference}->isa( 'Language::P::Value::Array' );
     return $self->{reference};
 }
 
 sub dereference_subroutine {
+    my( $self ) = @_;
+
     die unless $self->{reference}->isa( 'Language::P::Value::Subroutine' );
     return $self->{reference};
 }
 
 sub dereference_typeglob {
+    my( $self ) = @_;
+
     die unless $self->{reference}->isa( 'Language::P::Value::Typeglob' );
     return $self->{reference};
+}
+
+sub as_string {
+    my( $self ) = @_;
+    my $ref = $self->{reference};
+
+    my $prefix = $ref->isa( 'Language::P::Value::Reference' ) ? 'REF' :
+                 $ref->isa( 'Language::P::Value::Scalar' ) ? 'SCALAR' :
+                 $ref->isa( 'Language::P::Value::Hash' ) ? 'HASH' :
+                 $ref->isa( 'Language::P::Value::Array' ) ? 'ARRAY' :
+                 $ref->isa( 'Language::P::Value::Typeglob' ) ? 'GLOB' :
+                                                            die "$ref";
+
+    return sprintf '%s(0x%p)', $prefix, $ref;
 }
 
 sub as_boolean_int {
