51e65c6737ae06d864126353e35a43b1aeffeea2
Mattia Barbon <mbarbon@cpan.org>
2008-08-08 16:43:40 +0000

    Make "./Build test" run all tests and add test_parser, test_runtime, test_run, test_perl to run tests in a specific subdirectory.
    git-svn-id: file:///Users/mbarbon/devel/perl5/p/SVNREPOSITORY/language_p/trunk@111 200a23a6-3bc1-4d4e-9293-5d7e3368f848

--- inc/My/Build.pm
+++ inc/My/Build.pm
@@ -4,29 +4,71 @@ use strict;
 use warnings;
 use base qw(Module::Build);
 
-sub _run_with_p {
-    my( $self, $dirs ) = @_;
+sub _run_p_tests {
+    my( $self, @test_dirs ) = @_;
 
     require TAP::Harness;
+    require TAP::Formatter::Console;
+    require TAP::Parser::Aggregator;
 
-    my $harness =
-      TAP::Harness->new( { exec => [ $self->perl, '-Mblib', '--', 'bin/p' ],
-                           } );
-    my @base = $self->expand_test_dir( $dirs );
+    my $formatter = TAP::Formatter::Console->new( { jobs => 1 } );
+    my $aggregator = TAP::Parser::Aggregator->new;
+    $aggregator->start();
+    foreach my $test_dir ( @test_dirs ) {
+        my( $with_p, @directories ) = @$test_dir;
+        my $harness;
 
-    $harness->runtests( @base );
+        if( $with_p ) {
+            $harness = TAP::Harness->new
+              ( { formatter => $formatter,
+                  exec      => [ $self->perl, '-Mblib', '--', 'bin/p' ],
+                  } );
+        } else {
+            $harness = TAP::Harness->new
+              ( { formatter => $formatter,
+                  exec      => [ $self->perl, '-Mblib', '--' ],
+                  } );
+        }
+        my @tests = $self->expand_test_dir( @directories );
+
+        $harness->aggregate_tests( $aggregator, @tests );
+    }
+    $aggregator->stop();
+    $formatter->summary( $aggregator );
+}
+
+sub ACTION_test_parser {
+    my( $self ) = @_;
+
+    $self->_run_p_tests( [ 0, 't/parser' ] );
+}
+
+sub ACTION_test_runtime {
+    my( $self ) = @_;
+
+    $self->_run_p_tests( [ 0, 't/runtime' ] );
 }
 
 sub ACTION_test_perl {
     my( $self ) = @_;
 
-    $self->_run_with_p( 't/perl5' );
+    $self->_run_p_tests( [ 1, 't/perl5' ] );
 }
 
 sub ACTION_test_run {
     my( $self ) = @_;
 
-    $self->_run_with_p( 't/run' );
+    $self->_run_p_tests( [ 1, 't/run' ] );
+}
+
+sub ACTION_test {
+    my( $self ) = @_;
+
+    $self->_run_p_tests( [ 0, 't/parser' ],
+                         [ 0, 't/runtime' ],
+                         [ 1, 't/run' ],
+                         [ 1, 't/perl5' ],
+                         );
 }
 
 1;
