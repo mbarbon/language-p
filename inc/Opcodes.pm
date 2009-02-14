8778558b56909e56293d7ae9b1c028a89072cb6e
Mattia Barbon <mbarbon@cpan.org>
2009-02-14 18:42:22 +0100

    Add opcode descriptions for more opcodes.

--- inc/Opcodes.pm
+++ inc/Opcodes.pm
@@ -139,7 +139,7 @@ __DATA__
 abs                 0       noattr      same                 1   1
 add                 0       noattr      same                 2   1
 add_assign
-array_element
+array_element       0       noattr      same                 2   1
 array_length        0       noattr      array_size           1   1
 assign              0       noattr      same                 2   1
 backtick
@@ -147,23 +147,23 @@ binmode
 bit_and
 bit_or
 bit_xor
-call
+call                0       noattr      same                 2   1
 chdir               
 close               
-concat_assign
-concatenate         0       noattr      concat
+concat_assign       0       noattr      same                 2   1
+concatenate         0       noattr      concat               2   1
 constant_float      0       noattr      same                 0   1
 constant_integer    0       noattr      same                 0   1
 constant_regex      0       noattr      same                 0   1
-constant_string     0       noattr      same                 0   1
+constant_string     0       s           same                 0   1
 constant_sub        0       noattr      same                 0   1
 constant_undef      0       noattr      same                 0   1
-defined             
-dereference_array
-dereference_glob
-dereference_hash
-dereference_scalar
-dereference_sub     0       noattr      dereference_subroutine
+defined             0       noattr      same                 1   1
+dereference_array   0       noattr      same                 1   1
+dereference_glob    0       noattr      same                 1   1
+dereference_hash    0       noattr      same                 1   1
+dereference_scalar  0       noattr      same                 1   1
+dereference_sub     0       noattr      dereference_subroutine 1 1
 die                 
 divide
 divide_assign
@@ -172,7 +172,7 @@ dot_dot_dot
 dup
 end
 eval                
-fresh_string
+fresh_string        0       noattr      same                 0   1
 ft_atime
 ft_ctime
 ft_eexecutable
@@ -202,13 +202,13 @@ ft_setuid
 ft_sticky
 get
 glob                
-glob_slot           2       
-glob_slot_set
+glob_slot           0       noattr      same                 1   1
+glob_slot_set       0       noattr      same                 2   0
 global              0       noattr      same                 0   1
 grep                
-hash_element
-iterator
-iterator_next
+hash_element        0       noattr      same                 2   1
+iterator            0       noattr      same                 1   1
+iterator_next       0       noattr      same                 1   1
 jump
 jump_if_f_eq        0       noattr      same                 2   0
 jump_if_f_ge        0       noattr      same                 2   0
@@ -224,27 +224,26 @@ jump_if_s_le        0       noattr      same                 2   0
 jump_if_s_lt        0       noattr      same                 2   0
 jump_if_s_ne        0       noattr      same                 2   0
 jump_if_true        0       noattr      same                 1   0
-jump_if_undef       0       noattr      same                 1   0
-lexical
-lexical_clear
-lexical_set
-local
-localize_glob_slot
-log_and
+jump_if_null        0       noattr      same                 1   0
+lexical             0       noattr      same                 0   1
+lexical_clear       0       noattr      same                 0   0
+lexical_set         0       noattr      same                 1   0
+local               
+localize_glob_slot  0       noattr      same                 0   1
+log_and             0       noattr      same                 2   1
 log_not             0       noattr      not                  1   1
-log_or
-log_xor
-make_closure
-make_list           1       count       same                -1   1
+log_or              0       noattr      same                 2   1
+log_xor             0       noattr      same                 2   1
+make_closure        0       noattr      same                 1   1
+make_list           1       count=i     same                -1   1
 map                 
 match               0       noattr      rx_match
 minus               0       noattr      negate
 modulus
-multiply
+multiply            0       noattr      same                 2   1
 multiply_assign
 negate
 noop
-not
 not_match
 num_cmp
 num_eq              0       noattr      compare_f_eq_scalar
@@ -257,7 +256,7 @@ open
 parentheses
 phi
 pipe                
-plus
+plus                0       noattr      same                 1   1
 pop                 0       noattr      same                 1   0
 power
 print               0       noattr      same                 1   1
@@ -271,8 +270,8 @@ ql_tr
 readline            
 reference
 repeat
-restore_glob_slot
-return              
+restore_glob_slot   0       noattr      same                 0   0
+return              0       noattr      same                 1   0
 rmdir               
 set                 0       noattr      same                 2   0
 str_cmp
@@ -283,14 +282,14 @@ str_le              0       noattr      compare_s_le_scalar
 str_lt              0       noattr      compare_s_lt_scalar
 str_ne              0       noattr      compare_s_ne_scalar
 stringify
-subtract
+subtract            0       noattr      same                 2   1
 subtract_assign
-swap
-temporary           1       index
-temporary_set
+swap                0       noattr      same                 2   2
+temporary           0       index=i     same                 0   1
+temporary_set       0       noattr      same                 1   0
 undef               
 unlink              
-wantarray           0       noattr      want
+wantarray           0       noattr      want                 0   1
 
 rx_accept
 rx_capture_end
