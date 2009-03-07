b0c0fdd24051d0e68a88b82835056b90f25cc698
Mattia Barbon <mbarbon@cpan.org>
2009-03-07 15:49:01 +0100

    Bump version.

--- lib/Language/P.pm
+++ lib/Language/P.pm
@@ -33,7 +33,7 @@ use base qw(Class::Accessor::Fast);
 __PACKAGE__->mk_ro_accessors( qw(runtime parser generator) );
 __PACKAGE__->mk_accessors( qw(program program_arguments) );
 
-our $VERSION = '0.01_03';
+our $VERSION = '0.01_04';
 
 use Language::P::Parser;
 
