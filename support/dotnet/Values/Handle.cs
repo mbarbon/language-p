2281c4a9e79f90c29ba463d49015aa74de656ea9
Mattia Barbon <mbarbon@cpan.org>
2010-12-09 00:02:47 +0100

    Make P5Handle an IReferrable, not an IP5Any.

--- support/dotnet/Values/Handle.cs
+++ support/dotnet/Values/Handle.cs
@@ -3,18 +3,12 @@ using System.Collections.Generic;
 
 namespace org.mbarbon.p.values
 {
-    public class P5Handle : IP5Any
+    public class P5Handle : IP5Referrable
     {
         public P5Handle(Runtime runtime, System.IO.TextReader input, System.IO.TextWriter output)
         {
             Input = input;
             Output = output;
-        }
-
-        public virtual void Undef(Runtime runtime)
-        {
-            Input = null;
-            Output = null;
         }
 
         public int Write(Runtime runtime, IP5Any scalar, int offset, int length)
@@ -45,99 +39,9 @@ namespace org.mbarbon.p.values
             return line != null;
         }
 
-        // FIXME proper implementation
-        public virtual P5Scalar AsScalar(Runtime runtime) { throw new System.NotImplementedException(); }
-        public virtual string AsString(Runtime runtime) { throw new System.NotImplementedException(); }
-        public virtual int AsInteger(Runtime runtime) { throw new System.NotImplementedException(); }
-        public virtual double AsFloat(Runtime runtime) { throw new System.NotImplementedException(); }
-        public virtual bool AsBoolean(Runtime runtime) { return true; }
-        public virtual int StringLength(Runtime runtime) { throw new System.NotImplementedException(); }
-
-        public virtual P5Handle AsHandle(Runtime runtime)
-        {
-            return this;
-        }
-
-        public virtual int GetPos(Runtime runtime)
-        {
-            return -1;
-        }
-
-        public virtual int GetPos(Runtime runtime, out bool pos_set)
-        {
-            pos_set = false;
-
-            return -1;
-        }
-
-        public virtual IP5Any Clone(Runtime runtime, int depth)
-        {
-            return new P5Handle(runtime, Input, Output);
-        }
-
-        public virtual IP5Any Localize(Runtime runtime)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public virtual IP5Any Assign(Runtime runtime, IP5Any other)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public virtual IP5Any AssignIterator(Runtime runtime, IEnumerator<IP5Any> iter)
-        {
-            return Assign(runtime, iter.MoveNext() ? iter.Current : new P5Scalar(runtime));
-        }
-
         public virtual P5Scalar ReferenceType(Runtime runtime)
         {
             return new P5Scalar(runtime);
-        }
-
-        public virtual P5Scalar DereferenceScalar(Runtime runtime)
-        {
-            throw new System.InvalidOperationException("Not a reference");
-        }
-
-        public virtual P5Array DereferenceArray(Runtime runtime)
-        {
-            throw new System.InvalidOperationException("Not a reference");
-        }
-
-        public virtual P5Hash DereferenceHash(Runtime runtime)
-        {
-            throw new System.InvalidOperationException("Not a reference");
-        }
-
-        public virtual P5Typeglob DereferenceGlob(Runtime runtime)
-        {
-            throw new System.InvalidOperationException("Not a reference");
-        }
-
-        public virtual P5Code DereferenceSubroutine(Runtime runtime)
-        {
-            throw new System.InvalidOperationException("Not a reference");
-        }
-
-        public virtual P5Scalar VivifyScalar(Runtime runtime)
-        {
-            throw new System.InvalidOperationException("Not a reference");
-        }
-
-        public virtual P5Array VivifyArray(Runtime runtime)
-        {
-            throw new System.InvalidOperationException("Not a reference");
-        }
-
-        public virtual P5Hash VivifyHash(Runtime runtime)
-        {
-            throw new System.InvalidOperationException("Not a reference");
-        }
-
-        public virtual P5Code FindMethod(Runtime runtime, string method)
-        {
-            throw new System.InvalidOperationException("Not a reference");
         }
 
         public virtual void Bless(Runtime runtime, P5SymbolTable stash)
