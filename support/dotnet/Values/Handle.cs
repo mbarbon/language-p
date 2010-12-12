100a0dde1de9d5518491caa97b63bf6d0d26dda9
Mattia Barbon <mbarbon@cpan.org>
2010-12-12 18:53:17 +0100

    Fixed readline implementation.
      Avoids always adding a newline to the last
    file line.

--- support/dotnet/Values/Handle.cs
+++ support/dotnet/Values/Handle.cs
@@ -10,6 +10,9 @@ namespace org.mbarbon.p.values
         {
             input = _input;
             output = _output;
+
+            if (input != null)
+                read_buffer = new char[BUFFER_SIZE];
         }
 
         public int Write(Runtime runtime, IP5Any scalar, int offset, int length)
@@ -29,15 +32,58 @@ namespace org.mbarbon.p.values
 
         public bool Readline(Runtime runtime, out P5Scalar result)
         {
-            // TODO rewrite and optimize
-            var line = input.ReadLine();
+            System.Text.StringBuilder builder = null;
 
-            if (line == null)
-                result = new P5Scalar(runtime);
-            else
-                result = new P5Scalar(runtime, line + "\n");
+            for (;;)
+            {
+                if (rdbuf_start < rdbuf_end)
+                {
+                    int newline = System.Array.IndexOf(read_buffer, '\n', rdbuf_start, rdbuf_end - rdbuf_start);
 
-            return line != null;
+                    if (newline < 0 && rdbuf_end != BUFFER_SIZE)
+                        newline = rdbuf_end - 1;
+
+                    if (newline >= 0)
+                    {
+                        if (builder != null)
+                        {
+                            builder.Append(read_buffer, rdbuf_start, newline + 1 - rdbuf_start);
+
+                            result = new P5Scalar(runtime, builder.ToString());
+                        }
+                        else
+                            result = new P5Scalar(runtime, new string(read_buffer, rdbuf_start, newline + 1 - rdbuf_start));
+
+                        rdbuf_start = newline + 1;
+
+                        return true;
+                    }
+
+                    if (builder == null)
+                        builder = new System.Text.StringBuilder(2 * BUFFER_SIZE);
+
+                    builder.Append(read_buffer, rdbuf_start, rdbuf_end - rdbuf_start);
+                }
+
+                rdbuf_start = 0;
+                rdbuf_end = input.Read(read_buffer, 0, BUFFER_SIZE);
+
+                if (rdbuf_start == rdbuf_end)
+                {
+                    if (builder != null)
+                    {
+                        result = new P5Scalar(runtime, builder.ToString());
+
+                        return true;
+                    }
+                    else
+                    {
+                        result = new P5Scalar(runtime);
+
+                        return false;
+                    }
+                }
+            }
         }
 
         public bool Close(Runtime runtime)
@@ -87,7 +133,10 @@ namespace org.mbarbon.p.values
             return null;
         }
 
+        private int BUFFER_SIZE = 1024;
         private TextReader input;
         private TextWriter output;
+        private char[] read_buffer;
+        private int rdbuf_start, rdbuf_end;
     }
 }
