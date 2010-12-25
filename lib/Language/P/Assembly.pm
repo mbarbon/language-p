47d51957d22925e44f3c495ed774981f06443d60
Mattia Barbon <mbarbon@cpan.org>
2010-12-25 21:14:44 +0100

    Move the generic assembly instruction class to a separate file.

--- lib/Language/P/Assembly.pm
+++ lib/Language/P/Assembly.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use Exporter 'import';
 
+use Language::P::Instruction;
+
 our @EXPORT_OK = qw(label literal opcode opcode_n opcode_m opcode_nm
                     opcode_np opcode_npm);
 our %EXPORT_TAGS =
@@ -23,7 +25,7 @@ by backends.
 
 =cut
 
-sub i { Language::P::Assembly::Instruction->new( $_[0] ) }
+sub i { Language::P::Instruction->new( $_[0] ) }
 
 =head2 label
 
@@ -111,90 +113,6 @@ sub opcode_npm {
                pos        => $pos,
                attributes => %attributes ? \%attributes : undef,
                };
-}
-
-package Language::P::Assembly::Instruction;
-
-use strict;
-use warnings;
-use parent qw(Language::P::Object);
-
-__PACKAGE__->mk_ro_accessors( qw(label literal opcode opcode_n
-                                 parameters attributes) );
-
-use Scalar::Util; # blessed
-use Language::P::Constants qw(VALUE_SCALAR VALUE_ARRAY VALUE_HASH);
-
-my %sigil_to_name =
-  ( VALUE_SCALAR() => 'scalar',
-    VALUE_ARRAY()  => 'array',
-    VALUE_HASH()   => 'hash',
-    );
-
-sub _p {
-    my( $self, $arg, $name, $number_to_name, $attributes ) = @_;
-
-    return 'undef' unless defined $arg;
-
-    if( Scalar::Util::blessed( $arg ) ) {
-        return $arg->start_label
-            if $arg->isa( 'Language::P::Intermediate::BasicBlock' );
-        return '(' . substr( $arg->as_string( $number_to_name, $attributes ), 2, -1 ) . ')'
-            if $arg->isa( 'Language::P::Assembly::Instruction' );
-        return $sigil_to_name{$arg->sigil} . '(' . $arg->name . ')'
-            if $arg->isa( 'Language::P::ParseTree::LexicalDeclaration' );
-        return 'anoncode'
-            if $arg->isa( 'Language::P::Intermediate::Code' );
-    }
-    if( ref( $arg ) eq 'HASH' ) {
-        return '{' . join( ', ', map "$_ => $arg->{$_}",
-                                 sort keys %$arg ) . '}';
-    }
-    if(    $self->{opcode_n} && defined $name && $attributes
-        && (my $named = $attributes->{$self->{opcode_n}}{named}) ) {
-        my $type = $named->{$name};
-
-        if( $type && $type eq 's' ) {
-            ( my $v = $arg ) =~ s/([^\x20-\x7f])/sprintf "\\x%02x", ord $1/eg;
-
-            return qq{"$v"};
-        }
-    }
-
-    return $arg;
-}
-
-sub as_string {
-    my( $self, $number_to_name, $attributes ) = @_;
-
-    return $self->{literal} . "\n" if defined $self->{literal};
-
-    my $str = defined $self->{label} ? $self->{label} . ':' : ' ';
-
-    return $str . "\n" unless    defined $self->{opcode}
-                              || defined $self->{opcode_n};
-    $str .= ' ';
-
-    if( defined $self->{opcode} ) {
-        $str .= $self->{opcode};
-    } elsif( defined $self->{opcode_n} ) {
-        $str .= $number_to_name->{$self->{opcode_n}};
-    }
-
-    if( $self->{attributes} ) {
-        die "Can't happen" unless %{$self->{attributes}};
-        $str .= ' ' . join ', ',
-                      map  { "$_=" . _p( $self, $self->{attributes}{$_}, $_, $number_to_name, $attributes ) }
-                           sort keys %{$self->{attributes}};
-    }
-
-    if( $self->{parameters} ) {
-        die "Can't happen" unless @{$self->{parameters}};
-        $str .= ' ' . join ', ', map _p( $self, $_, undef, $number_to_name, $attributes ),
-                                     @{$self->{parameters}};
-    }
-
-    return $str . "\n";
 }
 
 1;
---
+++ lib/Language/P/Instruction.pm
@@ -0,0 +1,85 @@
+package Language::P::Instruction;
+
+use strict;
+use warnings;
+use parent qw(Language::P::Object);
+
+__PACKAGE__->mk_ro_accessors( qw(label literal opcode opcode_n
+                                 parameters attributes) );
+
+use Scalar::Util; # blessed
+use Language::P::Constants qw(VALUE_SCALAR VALUE_ARRAY VALUE_HASH);
+
+my %sigil_to_name =
+  ( VALUE_SCALAR() => 'scalar',
+    VALUE_ARRAY()  => 'array',
+    VALUE_HASH()   => 'hash',
+    );
+
+sub _p {
+    my( $self, $arg, $name, $number_to_name, $attributes ) = @_;
+
+    return 'undef' unless defined $arg;
+
+    if( Scalar::Util::blessed( $arg ) ) {
+        return $arg->start_label
+            if $arg->isa( 'Language::P::Intermediate::BasicBlock' );
+        return '(' . substr( $arg->as_string( $number_to_name, $attributes ), 2, -1 ) . ')'
+            if $arg->isa( 'Language::P::Instruction' );
+        return $sigil_to_name{$arg->sigil} . '(' . $arg->name . ')'
+            if $arg->isa( 'Language::P::ParseTree::LexicalDeclaration' );
+        return 'anoncode'
+            if $arg->isa( 'Language::P::Intermediate::Code' );
+    }
+    if( ref( $arg ) eq 'HASH' ) {
+        return '{' . join( ', ', map "$_ => $arg->{$_}",
+                                 sort keys %$arg ) . '}';
+    }
+    if(    $self->{opcode_n} && defined $name && $attributes
+        && (my $named = $attributes->{$self->{opcode_n}}{named}) ) {
+        my $type = $named->{$name};
+
+        if( $type && $type eq 's' ) {
+            ( my $v = $arg ) =~ s/([^\x20-\x7f])/sprintf "\\x%02x", ord $1/eg;
+
+            return qq{"$v"};
+        }
+    }
+
+    return $arg;
+}
+
+sub as_string {
+    my( $self, $number_to_name, $attributes ) = @_;
+
+    return $self->{literal} . "\n" if defined $self->{literal};
+
+    my $str = defined $self->{label} ? $self->{label} . ':' : ' ';
+
+    return $str . "\n" unless    defined $self->{opcode}
+                              || defined $self->{opcode_n};
+    $str .= ' ';
+
+    if( defined $self->{opcode} ) {
+        $str .= $self->{opcode};
+    } elsif( defined $self->{opcode_n} ) {
+        $str .= $number_to_name->{$self->{opcode_n}};
+    }
+
+    if( $self->{attributes} ) {
+        die "Can't happen" unless %{$self->{attributes}};
+        $str .= ' ' . join ', ',
+                      map  { "$_=" . _p( $self, $self->{attributes}{$_}, $_, $number_to_name, $attributes ) }
+                           sort keys %{$self->{attributes}};
+    }
+
+    if( $self->{parameters} ) {
+        die "Can't happen" unless @{$self->{parameters}};
+        $str .= ' ' . join ', ', map _p( $self, $_, undef, $number_to_name, $attributes ),
+                                     @{$self->{parameters}};
+    }
+
+    return $str . "\n";
+}
+
+1;
