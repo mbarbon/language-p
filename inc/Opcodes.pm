11da65b7a4a08edf5f2530864f6d5c0c74a0d64d
Mattia Barbon <mbarbon@cpan.org>
2010-12-25 21:44:47 +0100

    Name the opcode classes in the base opcodes file.

--- inc/Opcodes.pm
+++ inc/Opcodes.pm
@@ -25,7 +25,7 @@ sub parse_opdesc {
         $line =~ s/^\s+//; $line =~ s/\s+$//;
         next unless length $line;
         my( $opcode, $flags, $name, $in, $out, $attrs ) = split /\s+/, $line;
-
+        my $class;
         my $int_flags = 0;
         $int_flags |= $flag_map{$_} foreach split //, $flags || '0';
 
@@ -37,6 +37,7 @@ sub parse_opdesc {
 
             foreach ( split /,/, $attrs ) {
                 if( /(\w+)=(\w+)/ ) {
+                    $class = $2, next if $1 eq 'class';
                     $named{$1} = $2;
                     push @positional, $1, $2;
                 }
@@ -52,7 +53,7 @@ sub parse_opdesc {
         $out ||= 0;
         $opcode = 'OP_' . uc $opcode;
 
-        $op{$opcode} = [ $name, $in, $out, $attrs, $int_flags ];
+        $op{$opcode} = [ $name, $in, $out, $attrs, $int_flags, $class ];
 
         ++$num;
     }
@@ -363,12 +364,12 @@ add                 0       same                 2   1  context=i1
 add_assign          0       same                 2   1  context=i1
 anonymous_array     0       same                 1   1  noattr
 anonymous_hash      0       same                 1   1  noattr
-array_element       0       same                 2   1  context=i1,create=i1
+array_element       0       same                 2   1  context=i1,create=i1,class=ElementAccess
 array_length        0       array_size           1   1  context=i1
 array_pop           u       same                 1   1  context=i1
 array_push          0       same                 2   1  context=i1
 array_shift         u       same                 1   1  context=i1
-array_slice         0       same                 2   1  context=i1,create=i1
+array_slice         0       same                 2   1  context=i1,create=i1,class=ElementAccess
 array_unshift       0       same                 2   1  context=i1
 assign              0       same                 2   1  context=i1
 backtick            0       same                 1   1  context=i1
@@ -382,7 +383,7 @@ bit_xor             0       same                 2   1  context=i1
 bit_xor_assign      0       same                 2   1  context=i1
 bless               u       same                 2   1  context=i1
 call                0       same                 2   1  context=i1
-call_method         0       same                 1   1  context=i1,method=s
+call_method         0       same                 1   1  context=i1,method=s,class=CallMethod
 call_method_indirect 0      same                 2   1  context=i1
 caller              v       same                -1   1  context=i1,arg_count=i1
 chdir               u       same                 1   1  context=i1
@@ -390,11 +391,11 @@ chr                 u       same                 1   1  context=i1
 close               u       same                 1   1  context=i1
 concatenate         0       concat               2   1  context=i1
 concatenate_assign  0       concat_assign        2   1  context=i1
-constant_float      0       same                 0   1  value=f
-constant_integer    0       same                 0   1  value=i
-constant_regex      0       same                 0   1  value=c
-constant_string     0       same                 0   1  value=s
-constant_sub        0       same                 0   1  value=c
+constant_float      0       same                 0   1  value=f,class=ConstantFloat
+constant_integer    0       same                 0   1  value=i,class=ConstantInt
+constant_regex      0       same                 0   1  value=c,class=ConstantSub
+constant_string     0       same                 0   1  value=s,class=ConstantString
+constant_sub        0       same                 0   1  value=c,class=ConstantSub
 constant_undef      0       same                 0   1  noattr
 defined             u       same                 1   1  context=i1
 delete              u       same                 1   1  context=i1
@@ -419,12 +420,12 @@ dynamic_goto        u       same                 1   0  noattr
 each                u       same                 1   1  context=i1
 end                 0       same                 0   0  noattr
 eval                u       same                 1   1  context=i1,hints=i,warnings=su,package=s,lexicals=eval_my,globals=eval_our
-eval_regex          u       same                 1   1  context=i1,flags=i
+eval_regex          u       same                 1   1  context=i1,flags=i,class=RegexEval
 exists              u       same                 1   1  context=i1
 exists_array        u       same                 2   1  context=i1
 exists_hash         u       same                 2   1  context=i1
-find_method         u       same                 1   1  method=s
-fresh_string        0       same                 0   1  value=s
+find_method         u       same                 1   1  method=s,class=CallMethod
+fresh_string        0       same                 0   1  value=s,class=ConstantString
 ft_atime            u       same                 1   1  context=i1
 ft_ctime            u       same                 1   1  context=i1
 ft_eexecutable      u       same                 1   1  context=i1
@@ -452,57 +453,57 @@ ft_rwritable        u       same                 1   1  context=i1
 ft_setgid           u       same                 1   1  context=i1
 ft_setuid           u       same                 1   1  context=i1
 ft_sticky           u       same                 1   1  context=i1
-get                 0       same                 0   1  index=i,slot=i_sigil
+get                 0       same                 0   1  index=i,slot=i_sigil,class=GetSet
 glob                0       same                 1   1  context=i1
 glob_element        0       same                 2   1  context=i1
-glob_slot           0       same                 1   1  slot=i_sigil
-glob_slot_set       0       same                 2   0  slot=i_sigil
-global              0       same                 0   1  name=s,slot=i_sigil,context=i1
+glob_slot           0       same                 1   1  slot=i_sigil,class=GlobSlot
+glob_slot_set       0       same                 2   0  slot=i_sigil,class=GlobSlot
+global              0       same                 0   1  name=s,slot=i_sigil,context=i1,class=Global
 grep                0       same                 1   1  context=i1
-hash_element        0       same                 2   1  context=i1,create=i1
-hash_slice          0       same                 2   1  context=i1,create=i1
+hash_element        0       same                 2   1  context=i1,create=i1,class=ElementAccess
+hash_slice          0       same                 2   1  context=i1,create=i1,class=ElementAccess
 hex                 u       same                 1   1  context=i1
 index               v       same                -1   1  context=i1,arg_count=i1
 int                 u       same                 1   1  context=i1
 iterator            0       same                 1   1  noattr
 iterator_next       0       same                 1   1  noattr
 join                0       same                 1   1  context=i1
-jump                0       same                 0   0  to=b
-jump_if_f_eq        0       same                 2   0  to=b
-jump_if_f_ge        0       same                 2   0  to=b
-jump_if_f_gt        0       same                 2   0  to=b
-jump_if_f_le        0       same                 2   0  to=b
-jump_if_f_lt        0       same                 2   0  to=b
-jump_if_f_ne        0       same                 2   0  to=b
-jump_if_false       0       same                 1   0  to=b
-jump_if_null        0       same                 1   0  to=b
-jump_if_s_eq        0       same                 2   0  to=b
-jump_if_s_ge        0       same                 2   0  to=b
-jump_if_s_gt        0       same                 2   0  to=b
-jump_if_s_le        0       same                 2   0  to=b
-jump_if_s_lt        0       same                 2   0  to=b
-jump_if_s_ne        0       same                 2   0  to=b
-jump_if_true        0       same                 1   0  to=b
+jump                0       same                 0   0  to=b,class=Jump
+jump_if_f_eq        0       same                 2   0  to=b,class=Jump
+jump_if_f_ge        0       same                 2   0  to=b,class=Jump
+jump_if_f_gt        0       same                 2   0  to=b,class=Jump
+jump_if_f_le        0       same                 2   0  to=b,class=Jump
+jump_if_f_lt        0       same                 2   0  to=b,class=Jump
+jump_if_f_ne        0       same                 2   0  to=b,class=Jump
+jump_if_false       0       same                 1   0  to=b,class=Jump
+jump_if_null        0       same                 1   0  to=b,class=Jump
+jump_if_s_eq        0       same                 2   0  to=b,class=Jump
+jump_if_s_ge        0       same                 2   0  to=b,class=Jump
+jump_if_s_gt        0       same                 2   0  to=b,class=Jump
+jump_if_s_le        0       same                 2   0  to=b,class=Jump
+jump_if_s_lt        0       same                 2   0  to=b,class=Jump
+jump_if_s_ne        0       same                 2   0  to=b,class=Jump
+jump_if_true        0       same                 1   0  to=b,class=Jump
 keys                u       same                 1   1  context=i1
 lc                  u       same                 1   1  noattr
 lcfirst             u       same                 1   1  noattr
 length              u       same                 1   1  noattr
-lexical             0       same                 0   1  index=ls
-lexical_clear       0       same                 0   0  index=ls
-lexical_pad         0       same                 0   1  index=lp
-lexical_pad_clear   0       same                 0   0  index=lp
-lexical_pad_set     0       same                 1   0  index=lp
-lexical_set         0       same                 1   0  index=ls
-lexical_state_restore 0     same                 0   0  index=i
-lexical_state_save  0       same                 0   0  index=i
-lexical_state_set   0       same                 0   0  index=i
+lexical             0       same                 0   1  index=ls,class=Lexical
+lexical_clear       0       same                 0   0  index=ls,class=Lexical
+lexical_pad         0       same                 0   1  index=lp,class=Lexical
+lexical_pad_clear   0       same                 0   0  index=lp,class=Lexical
+lexical_pad_set     0       same                 1   0  index=lp,class=Lexical
+lexical_set         0       same                 1   0  index=ls,class=Lexical
+lexical_state_restore 0     same                 0   0  index=i,class=LexState
+lexical_state_save  0       same                 0   0  index=i,class=LexState
+lexical_state_set   0       same                 0   0  index=i,class=LexState
 list_slice          0       same                 2   1  context=i1
 local               0       same                 1   1  noattr
-localize_array_element 0    same                 2   1  index=i
-localize_glob_slot  0       same                 0   1  name=s,index=i,slot=i_sigil
-localize_hash_element 0     same                 2   1  index=i
-localize_lexical    0       same                 0   0  lexical=ls,index=i
-localize_lexical_pad 0      same                 0   0  lexical=lp,index=i
+localize_array_element 0    same                 2   1  index=i,class=LocalElement
+localize_glob_slot  0       same                 0   1  name=s,index=i,slot=i_sigil,class=LocalGlobSlot
+localize_hash_element 0     same                 2   1  index=i,class=LocalElement
+localize_lexical    0       same                 0   0  lexical=ls,index=i,class=LocalLexical
+localize_lexical_pad 0      same                 0   0  lexical=lp,index=i,class=LocalLexical
 log_and             0       same                 2   1  noattr
 log_and_assign      0       same                 2   1  context=i1
 log_not             0       not                  1   1  context=i1
@@ -514,7 +515,7 @@ make_closure        0       same                 1   1  noattr
 make_list           0       same                -1   1  context=i1
 make_qr             0       same                 1   1  noattr
 map                 0       same                 1   1  context=i1
-match               0       rx_match             2   1  context=i1,flags=i,index=i
+match               0       rx_match             2   1  context=i1,flags=i,index=i,class=RegexMatch
 minus               0       negate               1   1  context=i1
 modulus             0       same                 2   1  context=i1
 modulus_assign      0       same                 2   1  context=i1
@@ -562,20 +563,20 @@ repeat              0       same                 2   1  context=i1
 repeat_array        0       same                 2   1  context=i1
 repeat_assign       0       same                 2   1  context=i1
 repeat_scalar       0       same                 2   1  context=i1
-replace             0       rx_replace           2   1  context=i1,index=i,flags=i,to=b
+replace             0       rx_replace           2   1  context=i1,index=i,flags=i,to=b,class=RegexReplace
 require_file        u       same                 1   1  context=i1
-restore_array_element 0     same                 0   0  index=i
-restore_glob_slot   0       same                 0   0  name=s,index=i,slot=i_sigil
-restore_hash_element 0      same                 0   0  index=i
-restore_lexical     0       same                 0   0  lexical=ls,index=i
-restore_lexical_pad 0       same                 0   0  lexical=lp,index=i
+restore_array_element 0     same                 0   0  index=i,class=LocalElement
+restore_glob_slot   0       same                 0   0  name=s,index=i,slot=i_sigil,class=LocalGlobSlot
+restore_hash_element 0      same                 0   0  index=i,class=LocalElement
+restore_lexical     0       same                 0   0  lexical=ls,index=i,class=LocalLexical
+restore_lexical_pad 0       same                 0   0  lexical=lp,index=i,class=LocalLexical
 return              0       same                 1   0  context=i1
 reverse             0       same                 1   1  context=i1
 rmdir               u       same                 1   1  context=i1
 rx_split            v       same                -1   1  context=i1,arg_count=i1
 rx_split_skipspaces v       same                -1   1  context=i1,arg_count=i1
 scalar              u       same                 1   1  context=i1
-set                 0       same                 1   0  index=i,slot=i_sigil
+set                 0       same                 1   0  index=i,slot=i_sigil,class=GetSet
 shift_left          0       same                 2   1  context=i1
 shift_left_assign   0       same                 2   1  context=i1
 shift_right         0       same                 2   1  context=i1
@@ -596,10 +597,10 @@ substr              v       same                -1   1  context=i1,arg_count=i1
 subtract            0       same                 2   1  context=i1
 subtract_assign     0       same                 2   1  context=i1
 swap                0       same                 2   2  noattr
-temporary           0       same                 0   1  index=i,slot=i_sigil
-temporary_clear     0       same                 0   0  index=i,slot=i_sigil
-temporary_set       0       same                 1   0  index=i,slot=i_sigil
-transliterate       u       rx_transliterate     1   1  match=s,replacement=s,flags=i,context=i1
+temporary           0       same                 0   1  index=i,slot=i_sigil,class=Temporary
+temporary_clear     0       same                 0   0  index=i,slot=i_sigil,class=Temporary
+temporary_set       0       same                 1   0  index=i,slot=i_sigil,class=Temporary
+transliterate       u       rx_transliterate     1   1  match=s,replacement=s,flags=i,context=i1,class=RegexTransliterate
 uc                  u       same                 1   1  noattr
 ucfirst             u       same                 1   1  noattr
 undef               u       same                 1   0  noattr
@@ -612,25 +613,25 @@ vivify_scalar       0       same                 1   1  context=i1
 wantarray           u       want                 0   1  context=i1
 warn                0       same                 1   1  context=i1
 
-rx_accept           0       same                 0   0  groups=i
+rx_accept           0       same                 0   0  groups=i,class=RegexAccept
 rx_any              0       same                 0   0  noattr
 rx_any_nonewline    0       same                 0   0  noattr
-rx_backtrack        0       same                 0   0  to=b
+rx_backtrack        0       same                 0   0  to=b,class=RegexBacktrack
 rx_beginning        0       same                 0   0  noattr
-rx_capture_end      0       same                 0   0  group=i
-rx_capture_start    0       same                 0   0  group=i
-rx_class            0       same                 0   0  elements=s,ranges=s,flags=i
+rx_capture_end      0       same                 0   0  group=i,class=RegexCapture
+rx_capture_start    0       same                 0   0  group=i,class=RegexCapture
+rx_class            0       same                 0   0  elements=s,ranges=s,flags=i,class=RegexClass
 rx_end              0       same                 0   0  noattr
 rx_end_or_newline   0       same                 0   0  noattr
-rx_exact            0       same                 0   0  string=s,length=i
-rx_exact_i          0       same                 0   0  string=s,length=i
+rx_exact            0       same                 0   0  string=s,length=i,class=RegexExact
+rx_exact_i          0       same                 0   0  string=s,length=i,class=RegexExact
 rx_fail             0       same                 0   0  noattr
 rx_pop_state        0       same                 0   0  noattr
-rx_quantifier       0       same                 0   0  min=i,max=i,greedy=i1,group=i,to=b,subgroups_start=i,subgroups_end=i
-rx_restore_pos      0       same                 0   0  index=i
-rx_save_pos         0       same                 0   0  index=i
-rx_start_group      0       same                 0   0  to=b
+rx_quantifier       0       same                 0   0  min=i,max=i,greedy=i1,group=i,to=b,subgroups_start=i,subgroups_end=i,class=RegexQuantifier
+rx_restore_pos      0       same                 0   0  index=i,class=RegexState
+rx_save_pos         0       same                 0   0  index=i,class=RegexState
+rx_start_group      0       same                 0   0  to=b,class=RegexStartGroup
 rx_start_match      0       same                 0   0  noattr
-rx_state_restore    0       same                 0   0  index=i
-rx_try              0       same                 0   0  to=b
+rx_state_restore    0       same                 0   0  index=i,class=RegexState
+rx_try              0       same                 0   0  to=b,class=RegexTry
 rx_word_boundary    0       same                 0   0  noattr
