f61cbce95c1808609d9cee3412077d6954181553
Mattia Barbon <mbarbon@cpan.org>
2011-01-02 19:49:00 +0100

    Implemented global builtin overrides using CORE::GLOBAL.

--- lib/Language/P/Parser.pm
+++ lib/Language/P/Parser.pm
@@ -2292,10 +2292,12 @@ sub _declared_id {
     my( $self, $op ) = @_;
     my $call;
     my $opidt = $op->[O_ID_TYPE];
+    my $cg = is_overridable( $opidt ) && $self->runtime->is_declared( 'CORE::GLOBAL::' . $op->[O_VALUE], VALUE_SUB );
+    my $ov = is_overridable( $opidt ) && $self->runtime->is_declared( _qualify( $self, $op->[O_VALUE], $opidt ), VALUE_SUB );
 
     if(    is_overridable( $opidt )
-        && (    $op->[O_ID_FLAGS]
-             || !$self->runtime->is_declared( _qualify( $self, $op->[O_VALUE], $opidt ), VALUE_SUB ) ) ) {
+        && ( $op->[O_ID_FLAGS] || !$ov )
+        && !$cg ) {
         # all other overridable builtins
         if( $opidt == KEY_GLOB ) {
             $call = Language::P::ParseTree::Glob->new
@@ -2317,7 +2319,13 @@ sub _declared_id {
 
         return ( $call, 1 );
     } else {
-        my $fqname = _qualify( $self, $op->[O_VALUE], $opidt );
+        my $fqname;
+
+        if( $cg && !$ov ) {
+            $fqname = 'CORE::GLOBAL::' . $op->[O_VALUE];
+        } else {
+            $fqname = _qualify( $self, $op->[O_VALUE], $opidt );
+        }
 
         my $symbol = Language::P::ParseTree::Symbol->new
                          ( { name  => $fqname,
